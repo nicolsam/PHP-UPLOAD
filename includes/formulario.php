a414ae693621857fbd0810f5a9e5883325f88c9b
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-10 14:45:31 -0300

    Elementos HTML incluidos

--- includes/formulario.php
+++ includes/formulario.php
@@ -5,8 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload de Arquivos</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="content p-3">
+        <div id="welcome" class="mb-2">
+            <h1>Upload de arquivos</h1>
+        </div>
+        
+        <form method="post" enctype="multipart/form-data">
+            <div id="arquivo" class="mb-2">
+                <label for="file-upload">Upload de arquivos</label>
+                <input class="form-control" type="file" id="file-upload">
+            </div>
+            <div id="submit-button">
+                <button type="submit btn btn-success">Enviar arquivo</button>
+            </div>
+        </form>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+include __DIR__ . '/includes/formulario.php';
